189cdfe76acbb25663bb3674029b612933e6e0a1
janaalg <janatariq143@gmail.com>
2022-03-07 14:04:56 +0300

    instagram (there are a few things i did not know how to do)

--- ios-cw-5/ios-cw-5/ContentView.swift
+++ ios-cw-5/ios-cw-5/ContentView.swift
@@ -9,8 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        HStack{
+            
+            Image("i love food" )
+                .resizable()
+                .clipShape(Circle())
+                .frame(width: 150, height: 200)
+            
+            
+            Text("foodie")
+        
+          .font(.largeTitle)
+          .frame(width: 150, height: 200)
+        }
+        
+        HStack{
+            Image("pasta")
+                .resizable()
+                .frame(width: 130, height: 130, alignment: .center)
+            
+            Image("nachos")
+                .resizable()
+                .frame(width: 130, height: 130, alignment: .center)
+            
+            Image("taco")
+                .resizable()
+                .frame(width: 130, height: 130, alignment: .center)
+            
+        }
+        
+        HStack{
+            Image("burger")
+                .resizable()
+                .frame(width: 130, height: 130)
+            
+            Image("dounut")
+                .resizable()
+                .frame(width: 130, height: 130)
+            
+            Image("sushi")
+                .resizable()
+                .frame(width: 130, height: 130)
+            
+            
+        }
     }
 }
 
